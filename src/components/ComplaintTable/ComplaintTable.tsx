144f6885ad4dffd42d724699b81f6bde8af4b340
eman275 <eman.mohsen@tooli.tv>
2023-08-13 19:53:12 +0300

    some style in geid :

--- src/components/ComplaintTable/ComplaintTable.tsx
+++ src/components/ComplaintTable/ComplaintTable.tsx
@@ -1,49 +1,74 @@
-import { DataGrid, DataGridProps } from '@mui/x-data-grid';
-import { ComplaintDataList } from '../../data';
-import { GreenCell, RedCell, StyledDataGrid } from './StyledComplaintTable';
+import { DataGridProps } from "@mui/x-data-grid";
+import { ComplaintDataList } from "../../data";
+import {
+  ClosedCell,
+  ClosedWitCoupon,
+  ConvertedCell,
+  RedTextCell,
+  StyledDataGrid,
+  SuspendedCell,
+} from "./StyledComplaintTable";
 
 const ComplaintTable: React.FC = () => {
   const columns = [
-    { field: 'ComplaintNumber', headerName: 'Complaint Number', width: 180 },
-    { field: 'ComplaintDepartment', headerName: 'Complaint Department', width: 180 },
+    { field: "ComplaintNumber", headerName: "Complaint Number", width: 180 },
     {
-      field: 'ComplaintStatus',
-      headerName: 'Complaint Status',
+      field: "ComplaintDepartment",
+      headerName: "Complaint Department",
+      width: 180,
+    },
+    {
+      field: "ComplaintStatus",
+      headerName: "Complaint Status",
       width: 160,
-      renderCell: (params :any) => {
+      renderCell: (params: any) => {
         const complaintStatus = params.value;
-  
-        if (complaintStatus === 'معلقة') {
-          return <GreenCell>{complaintStatus}</GreenCell>;
-        } else if (complaintStatus === 'محولة') {
-          return <RedCell>{complaintStatus}</RedCell>;
+
+        if (complaintStatus === "معلقة") {
+          return <SuspendedCell>{complaintStatus}</SuspendedCell>;
+        } else if (complaintStatus === "محولة") {
+          return <ConvertedCell>{complaintStatus}</ConvertedCell>;
+        } else if (complaintStatus === "مغلقة") {
+          return <ClosedCell>{complaintStatus}</ClosedCell>;
+        } else if (complaintStatus === "مغلقة مع قسيمة") {
+          return <ClosedWitCoupon>{complaintStatus}</ClosedWitCoupon>;
         }
-  
+
         return complaintStatus;
       },
     },
-    { field: 'ComplaintDate', headerName: 'Complaint Date', width: 160 },
-    { field: 'ComplaintTimeFrame', headerName: 'Complaint Time Frame', width: 200 },
-    { field: 'TaxpayerName', headerName: 'Taxpayer Name', width: 180 },
+    {
+      field: 'ComplaintDate',
+      headerName: 'Complaint Date',
+      width: 160,
+      renderCell: (params: any) => <RedTextCell>{params.value}</RedTextCell>,
+      
+    },
+    {
+      field: "ComplaintTimeFrame",
+      headerName: "Complaint Time Frame",
+      width: 200,
+    },
+    { field: "TaxpayerName", headerName: "Taxpayer Name", width: 180 },
   ];
-  
 
   const dataGridProps: DataGridProps = {
     rows: ComplaintDataList,
     columns: columns,
     componentsProps: {
       pagination: {
-        page:5,
+        page: 5,
         // pageSize: 5,
         rowsPerPageOptions: [5, 10, 20],
       },
     },
+
     // disableSelectionOnClick: true,
   };
 
   return (
     // <div style={{ height: 400, width: '70%' }}>
-      <StyledDataGrid {...dataGridProps} />
+    <StyledDataGrid {...dataGridProps} />
     // </div>
   );
 };
--- src/data.ts
+++ src/data.ts
@@ -30,6 +30,16 @@ export const ComplaintDataList: ComplaintInterface[] = [
     },
     {
         id:Math.random(),
+        ComplaintNumber: 35218635214,
+        ComplaintDepartment: 'المكافحة',
+        ComplaintStatus: 'مغلقة مع قسيمة',
+                ComplaintDate: '2023/5/3',
+        ComplaintTimeFrame: '2023/5/3',
+        TaxpayerName: 'لوريم ايبسوم',
+    },
+
+    {
+        id:Math.random(),
         ComplaintNumber: 35218635216,
         ComplaintDepartment: 'الإفتراضي',
         ComplaintStatus: 'محولة',
@@ -51,6 +61,15 @@ export const ComplaintDataList: ComplaintInterface[] = [
         ComplaintNumber: 35218635216,
         ComplaintDepartment: 'الإفتراضي',
         ComplaintStatus: 'محولة',
+        ComplaintDate: '2023/5/3',
+        ComplaintTimeFrame: '2023/5/3',
+        TaxpayerName: 'لوريم ايبسوم',
+    },
+    {
+        id:Math.random(),
+        ComplaintNumber: 35218635214,
+        ComplaintDepartment: 'المكافحة',
+        ComplaintStatus: 'مغلقة مع قسيمة',
         ComplaintDate: '2023/5/3',
         ComplaintTimeFrame: '2023/5/3',
         TaxpayerName: 'لوريم ايبسوم',
